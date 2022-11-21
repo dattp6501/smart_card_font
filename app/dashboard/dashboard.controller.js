a1de6c51c68bd984196c8cf2172231a865b4bffa
dattp6501 <truongphucdat6501@gmail.com>
2022-11-22 00:37:32 +0700

    dattp

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -1,6 +1,6 @@
 SmartCardApp.controller('DashboardCtrl', ['$scope','$rootScope','$location','$cookies','DashboardService',
 function($scope,$rootScope,$location,$cookies,DashboardService){
     (function initController(){
-        $rootScope.sh_header = false;
+        $rootScope.sh_header = true;
     })();
 }]);
