4951067a2f9f10c8fda58f14e8a5ff57dee9a3cf
dattp6501 <truongphucdat6501@gmail.com>
2022-11-22 13:46:09 +0700

    dattp

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -8,7 +8,7 @@ function($scope,$rootScope,$location,$cookies,DashboardService){
             $location.path("/login");
         }
         var data1 = [{
-            name: 'Số làn ra vào bãi đỗ xe',
+            name: 'ra vào bãi đỗ xe',
             data: [43934, 48656, 65165, 81827, 112143, 142383,
                 171533, 165174, 155157, 161454, 154610]
         }, {
@@ -16,27 +16,42 @@ function($scope,$rootScope,$location,$cookies,DashboardService){
             data: [24916, 37941, 29742, 29851, 32490, 30282,
                 38121, 36885, 33726, 34243, 31050]
         }, {
-            name: 'Mục 2',
+            name: 'Mục 3',
             data: [11744, 30000, 16005, 19771, 20185, 24377,
                 32147, 30912, 29243, 29213, 25663]
         }, {
-            name: 'Mục 2',
+            name: 'Mục 4',
             data: [null, null, null, null, null, null, null,
                 null, 11164, 11218, 10077]
         }];
-        initLineChart(data1,"line-stats-user-chart1",'Biểu đồ thống kê theo từng tháng');
-
+        var xdata1 = {
+            "title": "Ngày",
+            "list": ['01/2022','02/2022','03/2022','04/2022','05/2022','06/2022','07/2022','08/2022','09/2022','10/2022','11/2022']
+        }
+        var yData1 = {
+            "title": "Số lượt",
+            "list": []
+        }
+        initLineChart(data1,"line-stats-user-chart1",'Biểu đồ thống kê theo từng tháng',xdata1,yData1);
+        //
         var data2 = [{
-            name: 'Số làn ra vào bãi đỗ xe',
+            name: 'Điểm',
             data: [43934, 48656, 65165, 81827, 112143, 142383,
                 171533, 165174, 155157, 161454, 154610]
         }];
-        initLineChart(data2,"line-stats-user-chart2",'Biểu đồ thống kê điểm theo từng tháng');
+        var xdata2 = {
+            "title": "Thời gian",
+            "list": ['01/2022','02/2022','03/2022','04/2022','05/2022','06/2022','07/2022','08/2022','09/2022','10/2022','11/2022']
+        }
+        var yData2 = {
+            "title": "Điểm",
+            "list": []
+        }
+        initLineChart(data2,"line-stats-user-chart2",'Biểu đồ thống kê điểm theo từng tháng',xdata2,yData2);
     })();
 
 
-    function initLineChart(data,id,title){
-        var xAxisData = ['01/2022','02/2022','03/2022','04/2022','05/2022','06/2022','07/2022','08/2022','09/2022','10/2022','11/2022'];
+    function initLineChart(data,id,title,xData,yData){
         Highcharts.chart(id, {
             chart:{
                 backgroundColor: '#ece5e5',
@@ -51,11 +66,11 @@ function($scope,$rootScope,$location,$cookies,DashboardService){
         
             yAxis: {
                 title: {
-                    text: 'số lượt'
+                    text: yData.title
                 }
             },
             xAxis: {
-                categories: xAxisData
+                categories: xData.list
             },
             legend: {
                 layout: 'vertical',
